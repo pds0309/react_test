241e85eadce095c5cb35b10524b886bd625962f3
pds0309 <ehd0309@gmail.com>
2022-09-05 15:06:57 +0900

    feat: 체크박스 체크 상태에 따라 버튼이 비활성화되는 기능 추가

--- color-button/src/App.js
+++ color-button/src/App.js
@@ -4,16 +4,22 @@ import { useState } from "react";
 
 function App() {
   const [color, setColor] = useState("red");
+  const [buttonDisabled, setBUttonDisabld] = useState(false);
+
   const newColor = color === "red" ? "blue" : "red";
   return (
     <div>
       <button
         style={{ backgroundColor: color }}
+        disabled={buttonDisabled}
         onClick={() => setColor(newColor)}
       >
         Change to {newColor}
       </button>
-      <input type="checkbox" />
+      <input
+        type="checkbox"
+        onChange={(event) => setBUttonDisabld(event.target.checked)}
+      />
     </div>
   );
 }

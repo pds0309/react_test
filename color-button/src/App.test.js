033ca89e724696118dc2800f7bc5cff5c0b60239
pds0309 <ehd0309@gmail.com>
2022-09-05 14:46:43 +0900

    add: button functional test failed

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -1,15 +1,21 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 
 import App from "./App";
 
-test("button has correct initial color", () => {
+test("button component functional test", () => {
   // RTL 의 render 를 사용해 렌더링 해야하는 컴포넌트를 선언한다.
   render(<App />);
   // RTL의 screen 전역 객체를 이용해 원하는 요소를 찾는다.
-  const button = screen.getByRole("button", { name: "Change to Blue" });
+  const button = screen.getByRole("button", { name: "Change to blue" });
   // I expect button's background color to be red;
   expect(button).toHaveStyle({ backgroundColor: "red" });
-});
-test("button turns a different color when clicked", () => {
-  render(<App />);
+
+  // generate click event
+  fireEvent.click(button);
+
+  // expect button's background color to be blue;
+  expect(button).toHaveStyle({ backgroundColor: "blue" });
+
+  // expect text to be changed
+  expect(button.textContent).toBe("Change to red");
 });

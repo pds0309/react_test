615f2abeeaf963cba7057cd9d33d647f1d00936c
pds0309 <ehd0309@gmail.com>
2022-09-06 17:43:40 +0900

    chore: 테스트 시 이벤트 처리를 위한 user-event 의존성 추가

--- icecreams/src/pages/summary/test/SummaryForm.test.jsx
+++ icecreams/src/pages/summary/test/SummaryForm.test.jsx
@@ -1,6 +1,7 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 import SummaryForm from "../SummaryForm";
+import userEvent from "@testing-library/user-event";
 
 test("동의 체크 박스는 기본적으로 체크되어있지 않다.", () => {
   render(<SummaryForm />);
@@ -10,7 +11,7 @@ test("동의 체크 박스는 기본적으로 체크되어있지 않다.", () =>
   expect(confirmCheckBox).not.toBeChecked();
 });
 
-test("동의 체크 박스가 체크된다면 주문 확인 버튼이 활성화 되며 체크 해제 시 다시 비활성화 된다.", () => {
+test("동의 체크 박스가 체크된다면 주문 확인 버튼이 활성화 되며 체크 해제 시 다시 비활성화 된다.", async () => {
   render(<SummaryForm />);
   const confirmCheckBox = screen.getByRole("checkbox", {
     name: /i agree to terms and conditions/i,
@@ -18,10 +19,10 @@ test("동의 체크 박스가 체크된다면 주문 확인 버튼이 활성화 
   const orderButton = screen.getByRole("button", { name: /confirm order/i });
 
   // 체크한다.
-  fireEvent.click(confirmCheckBox);
+  await userEvent.click(confirmCheckBox);
   expect(orderButton).toBeEnabled();
 
   // 체크 해제한다.
-  fireEvent.click(confirmCheckBox);
+  await userEvent.click(confirmCheckBox);
   expect(orderButton).toBeDisabled();
 });

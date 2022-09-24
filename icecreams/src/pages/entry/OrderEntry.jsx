9c63e41f0e864ca15be5a6da995671e755097401
pds0309 <ehd0309@gmail.com>
2022-09-24 20:20:45 +0900

    feat: OrderEntry 사용자 전역 옵션 상태 총계 테스팅, 출력 추가

--- icecreams/src/pages/entry/OrderEntry.jsx
+++ icecreams/src/pages/entry/OrderEntry.jsx
@@ -1,10 +1,13 @@
 import Options from "./Options";
+import { useOrderDetails } from "../../contexts/OrderDetails";
 
 const OrderEntry = () => {
+  const [orderDetails] = useOrderDetails();
   return (
     <div>
       <Options optionType="scoops" />
       <Options optionType="toppings" />
+      <h2>Grand total: ${orderDetails.totals["grandTotal"]}</h2>
     </div>
   );
 };
--- icecreams/src/pages/entry/test/totalUpdates.test.jsx
+++ icecreams/src/pages/entry/test/totalUpdates.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from "../../../test-utils/testing-library-utils";
 
 import Options from "../Options";
+import OrderEntry from "../OrderEntry";
 import userEvent from "@testing-library/user-event";
 
 test("scoops 옵션 변경으로 scoop 파트 총계가 변경된다", async () => {
@@ -50,4 +51,60 @@ test("toppings 옵션 체크여부로 topping 파트 총계가 변경된다.", a
 
   await userEvent.click(hotFudgeCheckbox);
   expect(toppingsTotal).toHaveTextContent("1.50");
+});
+
+describe("grand total test", () => {
+  test("grand total 은 최초에 $0.00 값을 가진다.", () => {
+    render(<OrderEntry />);
+    const grandTotal = screen.getByRole("heading", {
+      name: /Grand total: \$/i,
+    });
+    expect(grandTotal).toHaveTextContent("0.00");
+  });
+
+  test("scoops option 의 변동으로 grand total 의 값이 변경된다.", async () => {
+    render(<OrderEntry />);
+    const grandTotal = screen.getByRole("heading", {
+      name: /Grand total: \$/i,
+    });
+
+    const vanillaInput = await screen.findByRole("spinbutton", {
+      name: "Vanilla scoop",
+    });
+    userEvent.clear(vanillaInput);
+    await userEvent.type(vanillaInput, "1");
+
+    expect(grandTotal).toHaveTextContent("2.00");
+  });
+
+  test("toppings option 의 변동으로 grand total의 값이 변경된다.", async () => {
+    render(<OrderEntry />);
+    const grandTotal = screen.getByRole("heading", {
+      name: /Grand total: \$/i,
+    });
+
+    const hotFudgeCheckbox = await screen.findByRole("checkbox", {
+      name: "Hot fudge topping",
+    });
+    await userEvent.click(hotFudgeCheckbox);
+    expect(grandTotal).toHaveTextContent("1.50");
+  });
+
+  test("toppings 및 scoops 가 순차적으로 변동될 때 grand total 의 값이 변경된다,", async () => {
+    render(<OrderEntry />);
+    const grandTotal = screen.getByRole("heading", {
+      name: /Grand total: \$/i,
+    });
+
+    const hotFudgeCheckbox = await screen.findByRole("checkbox", {
+      name: "Hot fudge topping",
+    });
+    await userEvent.click(hotFudgeCheckbox);
+    const vanillaInput = await screen.findByRole("spinbutton", {
+      name: "Vanilla scoop",
+    });
+    userEvent.clear(vanillaInput);
+    await userEvent.type(vanillaInput, "1");
+    expect(grandTotal).toHaveTextContent("3.50");
+  });
 });

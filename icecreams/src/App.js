6add830f9067962670f6942e7f38508e05421a7b
pds0309 <ehd0309@gmail.com>
2022-09-25 14:25:56 +0900

    feat: App 주문요청 -> 주문확인 -> 주문완료

--- icecreams/src/App.js
+++ icecreams/src/App.js
@@ -1,15 +1,28 @@
 import "./App.css";
 
 import { Container } from "react-bootstrap";
+import OrderConfirmation from "./pages/confirmation/OrderConfirmation";
 import { OrderDetailsProvider } from "./contexts/OrderDetails";
 import OrderEntry from "./pages/entry/OrderEntry";
+import OrderSummary from "./pages/summary/OrderSummary";
+import { useState } from "react";
 
 function App() {
+  // orderPhase: inProgress, review, completed;
+  const [phase, setPhase] = useState("inProgress");
+
   return (
     <Container>
+      {phase}
       <OrderDetailsProvider>
         {/* summary page and entry page need provider */}
-        <OrderEntry />
+        {phase === "inProgress" ? (
+          <OrderEntry setPhase={setPhase} />
+        ) : phase === "review" ? (
+          <OrderSummary setPhase={setPhase} />
+        ) : (
+          <OrderConfirmation setPhase={setPhase} />
+        )}
       </OrderDetailsProvider>
       {/* confirmination page does not need provider */}
     </Container>
--- icecreams/src/App.test.js
+++
@@ -1,6 +0,0 @@
-import App from "./App";
-import { render } from "@testing-library/react";
-
-test("renders learn react link", () => {
-  render(<App />);
-});

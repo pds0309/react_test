6743c9b237b1479bb5045f3f8f353ed6195514c7
pds0309 <ehd0309@gmail.com>
2022-09-25 14:26:23 +0900

    feat: 주문 요청 post api mock handler 추가

--- icecreams/src/mock/handler.js
+++ icecreams/src/mock/handler.js
@@ -18,4 +18,8 @@ export const handlers = [
       ])
     );
   }),
+
+  rest.post("http://localhost:3030/order", (req, res, ctx) => {
+    return res(ctx.json({ orderNumber: 111111111 }));
+  }),
 ];

1754beb4d6c961229667832336e689eacb07547f
pds0309 <ehd0309@gmail.com>
2022-09-26 19:29:44 +0900

    feat: OrderEntry 선택된 Scoop이 없을 때 주문 버튼이 비활성화 되도록 수정

--- icecreams/src/pages/entry/OrderEntry.jsx
+++ icecreams/src/pages/entry/OrderEntry.jsx
@@ -1,15 +1,25 @@
 import { Button } from "react-bootstrap";
 import Options from "./Options";
+import { useEffect } from "react";
 import { useOrderDetails } from "../../contexts/OrderDetails";
+import { useState } from "react";
 
 const OrderEntry = ({ setPhase }) => {
   const [orderDetails] = useOrderDetails();
+  const [disabled, setDisabled] = useState(true);
+  useEffect(() => {
+    setDisabled(
+      ![...orderDetails.scoops.values()].filter((v) => v > 0).length > 0
+    );
+  }, [orderDetails]);
   return (
     <div>
       <Options optionType="scoops" />
       <Options optionType="toppings" />
       <h2>Grand total: {orderDetails.totals["grandTotal"]}</h2>
-      <Button onClick={() => setPhase("review")}>Order Sundae</Button>
+      <Button onClick={() => setPhase("review")} disabled={disabled}>
+        Order Sundae
+      </Button>
     </div>
   );
 };
--- icecreams/src/pages/entry/test/OrderEntry.test.jsx
+++ icecreams/src/pages/entry/test/OrderEntry.test.jsx
@@ -8,6 +8,7 @@ import { LAMBDA_URL } from "../../../constants";
 import OrderEntry from "../OrderEntry";
 import { rest } from "msw";
 import { server } from "../../../mock/server";
+import userEvent from "@testing-library/user-event";
 
 test("Scoop 및 Topping 라우트의 api 호출 장애를 핸들링한다.", async () => {
   //에러 상황을 위해 핸들러를 리셋한다.
@@ -24,4 +25,19 @@ test("Scoop 및 Topping 라우트의 api 호출 장애를 핸들링한다.", asy
     const alerts = await screen.findAllByRole("alert");
     expect(alerts).toHaveLength(2);
   });
+});
+
+test("Scoop 선택이 없으면 주문 버튼은 비활성화 상태이며 선택이 하나라도 있으면 활성화 된다", async () => {
+  render(<OrderEntry setPhase={jest.fn()} />);
+  const orderButton = screen.getByRole("button", { name: "Order Sundae" });
+  expect(orderButton).toBeInTheDocument();
+  expect(orderButton).toBeDisabled();
+
+  const chocolateInput = await screen.findByRole("spinbutton", {
+    name: "Chocolate scoop",
+  });
+  userEvent.clear(chocolateInput);
+  await userEvent.type(chocolateInput, "1");
+
+  expect(orderButton).toBeEnabled();
 });

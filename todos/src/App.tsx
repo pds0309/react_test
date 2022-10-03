3144a85bc0bc5c4af105ac60f770bba2a6783fc7
pds0309 <ehd0309@gmail.com>
2022-10-03 18:37:05 +0900

    feat: cra todo app

---
+++ todos/src/App.tsx
@@ -0,0 +1,7 @@
+import { Container } from "react-bootstrap";
+
+function App() {
+  return <Container className="App">hi</Container>;
+}
+
+export default App;
---
+++ todos/src/index.tsx
@@ -0,0 +1,10 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(<App />);

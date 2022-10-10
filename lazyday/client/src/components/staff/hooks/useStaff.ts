fcc5a4d1997e316dc5d9301bad1ea82277b296b9
pds0309 <ehd0309@gmail.com>
2022-10-10 17:30:26 +0900

    feat: useStaff staff fetching 기능 추가

--- lazyday/client/src/components/staff/hooks/useStaff.ts
+++ lazyday/client/src/components/staff/hooks/useStaff.ts
@@ -1,15 +1,15 @@
 import { Dispatch, SetStateAction, useState } from 'react';
+import { useQuery } from 'react-query';
 
 import type { Staff } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
 import { filterByTreatment } from '../utils';
 
-// for when we need a query function for useQuery
-// async function getStaff(): Promise<Staff[]> {
-//   const { data } = await axiosInstance.get('/staff');
-//   return data;
-// }
+async function getStaff(): Promise<Staff[]> {
+  const { data } = await axiosInstance.get('/staff');
+  return data;
+}
 
 interface UseStaff {
   staff: Staff[];
@@ -20,9 +20,8 @@ interface UseStaff {
 export function useStaff(): UseStaff {
   // for filtering staff by treatment
   const [filter, setFilter] = useState('all');
-
+  const fallback = [];
+  const { data: staff = fallback } = useQuery(queryKeys.staff, getStaff);
   // TODO: get data from server via useQuery
-  const staff = [];
-
   return { staff, filter, setFilter };
 }

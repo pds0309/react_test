3e7f67f7a24e243b7f92aef8897e98a34319e9b8
pds0309 <ehd0309@gmail.com>
2022-10-11 10:44:11 +0900

    feat: useStaff treatment 필터링 조회 기능 추가

--- lazyday/client/src/components/staff/hooks/useStaff.ts
+++ lazyday/client/src/components/staff/hooks/useStaff.ts
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction, useState } from 'react';
+import { Dispatch, SetStateAction, useCallback, useState } from 'react';
 import { useQuery } from 'react-query';
 
 import type { Staff } from '../../../../../shared/types';
@@ -18,10 +18,14 @@ interface UseStaff {
 }
 
 export function useStaff(): UseStaff {
-  // for filtering staff by treatment
   const [filter, setFilter] = useState('all');
+  const selectFunc = useCallback((data) => filterByTreatment(data, filter), [
+    filter,
+  ]);
   const fallback = [];
-  const { data: staff = fallback } = useQuery(queryKeys.staff, getStaff);
-  // TODO: get data from server via useQuery
+  const { data: staff = fallback } = useQuery(queryKeys.staff, getStaff, {
+    select: filter === 'all' ? undefined : selectFunc,
+  });
+
   return { staff, filter, setFilter };
 }

118ac5c677820ed7742fb8102c36237f7c3a0a01
pds0309 <ehd0309@gmail.com>
2022-10-12 19:27:05 +0900

    test: Calendar api 패치 실패 시 테스트 추가

--- lazyday/client/src/components/appointments/tests/Calendar.test.tsx
+++ lazyday/client/src/components/appointments/tests/Calendar.test.tsx
@@ -1,9 +1,8 @@
 import { screen } from '@testing-library/react';
+import { rest } from 'msw';
+import { renderWithQueryClient } from 'test-utils';
 
-// import { rest } from 'msw';
-// import { server } from '../../../mocks/server';
-// import { defaultQueryClientOptions } from '../../../react-query/queryClient';
-// import { renderWithClient } from '../../../test-utils';
+import { server } from '../../../mocks/server';
 import { Calendar } from '../Calendar';
 
 // mocking useUser to mimic a logged-in user
@@ -12,14 +11,18 @@ import { Calendar } from '../Calendar';
 //   useUser: () => ({ user: mockUser }),
 // }));
 
-test('Reserve appointment error', () => {
+test('Reserve appointment error', async () => {
   // (re)set handler to return a 500 error for appointments
-  // server.resetHandlers(
-  //   rest.get(
-  //     'http://localhost:3030/appointments/:month/:year',
-  //     (req, res, ctx) => {
-  //       return res(ctx.status(500));
-  //     },
-  //   ),
-  // );
+  server.resetHandlers(
+    rest.get(
+      'http://localhost:3030/appointments/:month/:year',
+      (req, res, ctx) => {
+        return res(ctx.status(500));
+      },
+    ),
+  );
+  renderWithQueryClient(<Calendar />);
+
+  const alertToast = await screen.findByRole('alert');
+  expect(alertToast).toHaveTextContent(/500/i);
 });
